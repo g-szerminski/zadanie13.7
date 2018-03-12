943a6b5d8afc25604b24ebbe8b84b63ac91411fe
Grzegorz Szermiński <szerman@poczta.fm>
2018-03-12 11:31:05 +0100

    Change text colors

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ var colors = require('colors');
 //});
 
 fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
-	console.log('Dane przed zapisem!'.blue);
+	console.log('Dane przed zapisem!'.green);
 	console.log(data);
 	fs.appendFile('./tekst.txt', '\nA tak wyglądają po zapisie!', function(err) {
 		if (err) throw err;
 		console.log('Zapisano!'.blue);
 		fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
-			console.log('Dane po zapisie'.blue)
+			console.log('Dane po zapisie'.red)
 			console.log(data);
 		});
 	});
